cd10233a173d1ad95183a44ebb5e6bebcc26fab3
huangjinwei <zsjinwei@foxmail.com>
2020-04-03 19:10:25 +0800

    update

--- www/cacheclear.js
+++ www/cacheclear.js
@@ -2,10 +2,10 @@ var exec = require('cordova/exec');
 
 var cacheClear = {
   getCacheSize: function(success, error) {
-    exec(success, error, 'Cache', 'getCacheSize', []);
+    exec(success, error, 'cacheclear', 'getCacheSize', []);
   },
   clearCache: function(success, error) {
-    exec(success, error, 'Cache', 'clearCache', []);
+    exec(success, error, 'cacheclear', 'clearCache', []);
   }
 };
 
